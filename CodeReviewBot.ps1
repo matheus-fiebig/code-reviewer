644e9be069537e969bc8026ece19fb809bad3671
Matheus Fiebig <macrubig@gmail.com>
2024-04-05 00:42:21 -0300

    added bar

--- CodeReviewBot.ps1
+++ CodeReviewBot.ps1
@@ -2,5 +2,4 @@ $configPath = './config.json'
 $configJson = Get-Content config.json | ConvertFrom-Json
 echo $configJson.mainBranch
 
-
-git diff --name-status firstbranch..yourBranchName
+git diff --name-status main

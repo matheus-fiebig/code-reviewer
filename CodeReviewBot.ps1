6eb6d90a7e27c9b0ded11b2912e250e7c0a39fc5
Matheus Fiebig <macrubig@gmail.com>
2024-04-05 23:19:12 -0300

    removed test files

--- CodeReviewBot.ps1
+++ CodeReviewBot.ps1
@@ -4,11 +4,15 @@ $config = Get-Content $configPath | ConvertFrom-Json
 $allowedSuffix = $config.models.allowedSuffix;
 $path = $config.models.path;
 
-#WrongNamingRule
-$wrongNamedFiles = 
-    git diff --name-status $config.targetBranch |
-            Select-String -Pattern "[^0]+(?=$path)+(?=(?!.*$allowedSuffix.cs$))"
-
 Clear-Host
+
+#WrongNamingRule
 Write-Host "   Wrong Named Models   " -ForegroundColor white -BackgroundColor darkred
-Write-Host $wrongNamedFiles -ForegroundColor darkred 
+foreach ($namingRule in $config.namingRules)
+{
+    $wrongNamedFiles = git diff --name-status $config.targetBranch |
+            Select-String -Pattern "[^0]+(?=$path)+(?=(?!.*$allowedSuffix.cs$))"
+    Write-Host $wrongNamedFiles -ForegroundColor darkred 
+}
+
+Write-Host "   Remember to always check for rich domain and variable names   " -ForegroundColor white -BackgroundColor darkyellow
